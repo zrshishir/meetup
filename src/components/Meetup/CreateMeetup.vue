71725e360c2331d98d947a374f9c9238d66aeca3
zrshishir <zrshishir@gmail.com>
2018-10-30 20:09:50 +0600

    meetup page creation

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,74 @@
 <template>
-    <div>
-        <p>this is create meetup page</p>
-    </div>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm-6 offset-sm3>
+                <h2 class="purple--text">Create a meetup</h2>
+            </v-flex>
+        </v-layout>
+        <v-layout>
+            <v-flex xs12>
+                <form @submit.prevent="onCreateMeetup">
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-text-field name="title" label="Title" v-model="title" id="title" color="purple"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-text-field name="location" label="Location" v-model="location" id="location" color="purple"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-text-field name="imageUrl" label="imageUrl" v-model="imageUrl" id="image-url" color="purple"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-img :src="imageUrl"></v-img>
+                        </v-flex>
+                    </v-layout>
+                     <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-text-field name="description" label="Description" v-model="description" id="description" multi-line color="purple"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-btn flat dark class="purple darken3" type="submit" :disabled="!meetupForm">Create Meetup</v-btn>
+                        </v-flex>
+                    </v-layout>
+                </form>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: "",
+                location: "",
+                imageUrl: "",
+                description: "",
+            }
+        },
+        computed:{
+            meetupForm(){
+                return this.title != '' && this.location != '' && this.imageUrl != '' && this.description != ''
+            }
+        }, 
+        methods: {
+            onCreateMeetup(){
+                const meetupData = {
+                    title: this.title,
+                    location: this.location,
+                    imageUrl: this.imageUrl,
+                    description: this.description,
+                    date: new Date()
+                }
+            }
+        }
+    }
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,14 @@ export const store = new Vuex.Store({
                 id: '1', 
                 imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/Our_Bangladesh.jpg', 
                 title: 'Village, Bangladesh', 
+                location: 'Bangladesh',
                 date: '2018-10-29'
             },
             { 
                 id: '2', 
                 imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Natural_beauty_of_bangladesh_village.JPG', 
                 title: 'Nature, Bangladesh', 
+                location: 'Bangladesh',
                 date: '2018-10-30'
             }
         ], 
@@ -28,11 +30,23 @@ export const store = new Vuex.Store({
     }, 
 
     mutations: {
-
+        createMeetup(state, payload){
+            state.loadedMeetups.push(payload)
+        }
     },
 
     actions: {
-
+        createMeetup({ commit }, payload){
+            const meetup = {
+                title = payload.title,
+                location = payload.location,
+                imageUrl = payload.imageUrl,
+                description = payload.description,
+                date = payload.date
+            }
+            //Reach out to firebase and store it
+            commit('createMeetup', meetup)
+        }
     }, 
 
     getters: {
